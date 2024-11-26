eead22acd95dc893896b66fa5b884d81f825e1bc
Sersoc <45021157+Sersoc@users.noreply.github.com>
2024-11-27 00:55:11 +0900

    Update

--- calculate.go
+++ calculate.go
@@ -1,32 +1,16 @@
 package lcmgcdhelper
 
-import "fmt"
-
 func CalculateGCD(x int, y int) int {
-	var low int
-	var high int
-	if x > y {
-		high = x
-		low = y
-	} else {
-		high = y
-		low = x
+	if y == 0 {
+		return x
 	}
-	if low == 0 {
-		return high
-
-	}
-
-	return CalculateGCD(low, high%low)
-
+	return CalculateGCD(y, x%y)
 }
 
 func CalculateLCM(x int, y int) int {
-	defer func() {
-		e := recover()
-		fmt.Println(e)
-	}()
+	if x == 0 || y == 0 {
+		return 0
+	}
 	z := CalculateGCD(x, y)
-
-	return (x / z) * (y / z) * z
+	return (x / z) * y
 }
---
+++ go.mod 00-03-28-950.mod
@@ -0,0 +1,3 @@
+module github.com/Sersoc/LCM
+
+go 1.19
